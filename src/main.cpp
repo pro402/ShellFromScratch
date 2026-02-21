9fe3b5ef39d4c0480678215d8b6efd4d2b004194
pro402 <maitypro402@gmail.com>
2026-02-21 09:26:29 +0530

    adding command invalid check

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 int main(){
     std::cout<<"$ ";
     std::string input;
+    // we will treat every command as invalid here except exit for loop
     while(getline(std::cin, input)){
-        if(input != "exit") std::cout<<"$ ";
+        if(input != "exit") {
+            if(input!="") std::cout<<input<<": command not found"<<std::endl;
+            std::cout<<"$ ";
+        }
         else break;
     }
 }
