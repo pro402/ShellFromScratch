e17b1379de7ec2d31fa00f19f4e86ff9ca2ee580
pro402 <maitypro402@gmail.com>
2026-02-21 10:29:29 +0530

    Version 1.1 (adding echo command)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+
+void checkEcho(std::string input){}
 
 int main(){
     std::cout<<"$ ";
@@ -6,8 +9,22 @@ int main(){
     // we will treat every command as invalid here except exit for loop
     while(getline(std::cin, input)){
         if(input != "exit") {
-            if(input!="") std::cout<<input<<": command not found"<<std::endl;
-            std::cout<<"$ ";
+            // input manager
+            std::size_t pos_echo = input.find("echo ");
+            if(pos_echo != std::string::npos){
+                std::string args = input.substr(pos_echo+5);
+                std::cout<<args<<std::endl;
+                std::cout<<"$ ";
+                continue;
+            }
+            else if(input == "") {
+                std::cout<<"$ ";
+                continue;
+            }
+            else {
+                std::cout<<input<<": command not found"<<std::endl;
+                std::cout<<"$ ";
+            }
         }
         else break;
     }
